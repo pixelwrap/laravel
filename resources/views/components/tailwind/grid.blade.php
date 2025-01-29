838d8cf44202a83bf35f4cfa82eb028d98c94c78
Griffins Oringo <oringogriffins78@gmail.com>
2025-01-29 21:04:58 +0300

    wip

--- resources/views/components/tailwind/grid.blade.php
+++ resources/views/components/tailwind/grid.blade.php
@@ -2,11 +2,29 @@
     if(!isset($grid)){
         raise(null, "You must pass \"grid\" when rendering the grid component");
     }
+    $gridErrors = [];
     if(!isset($grid->nodes)){
-        $gridErrors = ["Nodes must be set. Please check if your template is formated correctly."];
+        $gridErrors[] = "Nodes must be set. Please check if your template is formated correctly.";
     }else{
-        $columns = $columnOptions[$grid->cols ?? 12];
+        $columns = $grid->cols ?? 12;
+        $validations= [
+            "columns" => array_keys($columnOptions)
+        ];
+
+        foreach($grid->nodes as $index => $node) {
+            $validations["Grid Node:" . $index] = range(1,12);
+        }
+        foreach ($validations as $key => $options){
+            if(!in_array($$key, $options)){
+                $buttonErrors[] = sprintf("\"%s\" only allows one of %s.", mb_ucfirst($key) , implode(", ", $options));
+            }
+        }
+
         [$gridErrors, $border, $margin, $padding, $gap] = parseBoxModelProperties($grid, get_defined_vars());
+    }
+
+    if(count($gridErrors) ===0 ){
+        $columns = $columnOptions[$columns];
     }
 @endphp
 @if(count($gridErrors)>0)
