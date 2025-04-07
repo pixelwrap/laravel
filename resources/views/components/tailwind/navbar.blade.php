7468d6fc15efb9cdd444c16984270c4cc4244cc1
Griffins Oringo <oringogriffins78@gmail.com>
2025-04-08 02:24:51 +0300

    wip

--- resources/views/components/tailwind/navbar.blade.php
+++ resources/views/components/tailwind/navbar.blade.php
@@ -8,7 +8,7 @@
                             @class([
                                'px-2 py-2 text-sm font-bold flex flex-row gap-x-2 items-center border-b-4 ',
                                'text-gray-900 border-gray-900 dark:border-gray-50 dark:text-gray-50 hover:text-white dark:hover:text-gray-50 dark:hover:bg-gray-950 hover:bg-gray-600' => $item->selected,
-                               'text-gray-950 dark:bg-gray-900 dark:border-gray-900 hover:border-gray-200 dark:hover:border-gray-800 border-gray-100 dark:text-gray-300 hover:text-white dark:hover:text-gray-950 dark:hover:bg-gray-200 hover:bg-gray-950' => !$item->selected])>
+                               'text-gray-950 dark:bg-gray-900 dark:border-gray-900 hover:border-gray-600 dark:hover:border-gray-500 border-gray-100 dark:text-gray-300 hover:text-white dark:hover:text-gray-950 dark:hover:bg-gray-200 hover:bg-gray-950' => !$item->selected])>
                             @if($item->icon)
                                 <span>@pixelicon($item->icon)</span>
                             @endif
