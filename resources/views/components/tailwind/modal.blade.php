0e03e8fc7e1d2e68cadfab116b24d7bd76de8cbf
Griffins Oringo <oringogriffins78@gmail.com>
2025-04-08 18:33:13 +0300

    wip

--- resources/views/components/tailwind/modal.blade.php
+++ resources/views/components/tailwind/modal.blade.php
@@ -6,7 +6,7 @@
                     {{$modal->text(get_defined_vars())}}
                 </h3>
                 @if($modal->showClose)
-                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 {{$modal->roundClasses ?? ''}} text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="{{$modal->id}}">
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 {{$modal->roundClasses ?? ''}} text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="{{$modal->id(get_defined_vars())}}">
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                         </svg>
