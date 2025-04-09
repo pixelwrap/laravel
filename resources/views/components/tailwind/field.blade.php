b657bab595201ca1587aa17dc03c23103582da4b
Griffins Oringo <oringogriffins78@gmail.com>
2025-04-09 17:08:20 +0300

    wip

--- resources/views/components/tailwind/field.blade.php
+++ resources/views/components/tailwind/field.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col">
-    <p class="p-0 text-gray-800 dark:text-gray-50 {{$field->classes}}">
+    <p class="p-0 text-gray-800 dark:text-gray-50 {{$field->classes}} font-bold">
         {{$field->text(get_defined_vars())}}
     </p>
     <p class="p-0 text-gray-800 dark:text-gray-50 text-sm">
