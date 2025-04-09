fdccfb69f6ff4f4a51ea1ef16bc6689c860baf29
Griffins Oringo <oringogriffins78@gmail.com>
2025-04-09 14:36:07 +0300

    wip

---
+++ src/Components/Field.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PixelWrap\Laravel\Components;
+
+use PixelWrap\Laravel\Traits\HasText;
+
+class Field extends Text
+{
+    use HasText;
+
+    public string $value = "";
+    public string $default = "";
+
+    public function parseProps($node, $data): void
+    {
+        parent::parseProps($node, $data);
+        $this->default = $node->default ?? $this->default;
+        $this->value   = $this->node->value ?? $this->default;
+    }
+
+    public function value($args = [])
+    {
+        return interpolateString($this->value, [...$args, ...$this->data]);
+    }
+}
