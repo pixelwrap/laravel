d5090483819f40744cf72ecad7c7ef1af17d13fb
Griffins Oringo <oringogriffins78@gmail.com>
2025-04-09 18:11:44 +0300

    wip

--- resources/views/components/tailwind/badge.blade.php
+++ resources/views/components/tailwind/badge.blade.php
@@ -1,3 +1,3 @@
-<span class="  {{ $badge->classes }} inline-block border border-1 border-gray-600 dark:border-gray-50 bg-gray-200 dark:bg-gray-950 px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-50">
+<span class="{{ $badge->classes }} inline-block border border-1 border-gray-600 dark:border-gray-50 bg-gray-200 dark:bg-gray-950 px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-50">
     {{ $badge->text(get_defined_vars()) }}
 </span>
