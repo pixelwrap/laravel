54838311cae49fea318b9b084b102dd9c75f8436
Griffins Oringo <oringogriffins78@gmail.com>
2025-04-09 17:38:53 +0300

    wip

--- resources/views/components/tailwind/tab.blade.php
+++ resources/views/components/tailwind/tab.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row flex-grow {{ $tab->classes }}">
+<div class="flex flex-column {{ $tab->classes }}">
     @foreach($tab->nodes as $component)
         {{ $component->render(get_defined_vars()) }}
     @endforeach
