768d19b4873a16be6d2e220f8f31f0ecbebdfae5
Griffins Oringo <oringogriffins78@gmail.com>
2025-04-14 22:01:50 +0300

    overflow hidden for cards

--- resources/views/components/tailwind/card.blade.php
+++ resources/views/components/tailwind/card.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700 {{ $card->classes }} {{$card->roundModeratedClasses}}">
+<div class="w-full bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700 overflow-hidden {{ $card->classes }} {{$card->roundModeratedClasses}}">
     @if($card->showLabel)
         <div class="flex items-center justify-between p-2">
             {{ \PixelWrap\Laravel\PixelWrapRenderer::from(get_defined_vars(),['type' => 'heading','label' => $card->text(get_defined_vars())], $card->theme)->render() }}
