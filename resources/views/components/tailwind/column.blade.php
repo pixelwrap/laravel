d4ba0ff805aff7ad93091c6bdb241b9609281bd3
Griffins Oringo <oringogriffins78@gmail.com>
2025-04-16 19:38:27 +0300

    wip

--- resources/views/components/tailwind/column.blade.php
+++ resources/views/components/tailwind/column.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col flex-grow align-left {{$column->classes}}">
+<div class="flex flex-col flex-grow align-left h-full {{$column->classes}}">
     @foreach($column->nodes as $component)
         {{ $component->render(get_defined_vars()) }}
     @endforeach
